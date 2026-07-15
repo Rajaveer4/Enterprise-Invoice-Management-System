034d2f09761c5398afd926b94702fb7e7863c77f
Rajaveer Patil <rajveer.patil.rr@gmail.com>
2026-07-15 11:20:30 +0530

    Delete src/pages directory

--- src/pages/HomePage.jsx
+++
@@ -1,8 +0,0 @@
-import DashboardPage from './DashboardPage';
-import VendorPortalPage from './VendorPortalPage';
-import { useAuth } from '../context/AuthContext';
-
-export default function HomePage() {
-  const { role } = useAuth();
-  return role === 'VENDOR' ? <VendorPortalPage /> : <DashboardPage />;
-}
